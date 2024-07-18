9f34b236d746c9709e9ebc9f4d7361549fcfcd64
Saikishen <128302556+Saiikishen@users.noreply.github.com>
2024-07-19 00:53:20 +0530

    Update findDuplicate.py

--- findDuplicate.py
+++ findDuplicate.py
@@ -8,3 +8,8 @@ class Solution:
                 return num
             else:
                 num_count[num] = 1
+
+
+# Dictionary (Hashmap) Usage: The dictionary num_count is used to keep track of the occurrences of each number.
+# Efficiency: The solution iterates through the list only once, making it O(n) in terms of time complexity. The space complexity is O(n) due to the storage needed for the dictionary.
+# Return First Duplicate: The method returns as soon as it finds the first duplicate number, ensuring optimal performance.
