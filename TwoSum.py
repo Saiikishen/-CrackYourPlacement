d805d800f97031dd55d70ba71d928ca583d6f3b6
Saikishen <128302556+Saiikishen@users.noreply.github.com>
2024-07-20 22:29:19 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
+        num_index = {}
         
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in num_to_index:
-                return [num_to_index[complement], i]
+                return [num_index[complement], i]
             
 
-            num_to_index[nums[i]] = i
+            num_index[nums[i]] = i
+
+
+#Empty dictionary num_index is initialised
+#Complement of the numbers in nums is calculated
+#If complement exists in num_index then index of num and complement is returned
+#else store it in dict with the index of num[i]
